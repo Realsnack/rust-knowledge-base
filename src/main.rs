7d4e42484b49f08251f0276b2b37025502998b5c
Michal Svačina <msvacina@outlook.com>
2023-05-25 18:48:23 +0200

    Added postgres example

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
+use person::Person;
+use postgres::{Client, NoTls};
+
 pub mod person;
 
 fn main() {
-    bincode_example();
-}
-
-
-fn bincode_example() {
     let person = person::Person {
         name: "Alice".to_string(),
         age: 20,
     };
 
+    bincode_example(&person);
+
+    postgres_example(&person);
+}
+
+fn bincode_example(person: &Person) {
     let encoded: Vec<u8> = bincode::serialize(&person).unwrap();
     println!("encoded: {:?}", encoded);
 
     let decoded: person::Person = bincode::deserialize(&encoded[..]).unwrap();
     println!("decoded: {:?}", decoded);
+}
+
+fn postgres_example(person: &Person) {
+    let mut client = Client::connect(
+        "postgresql://rust_app:karel123@centos01.vs.msvacina.cz/rust",
+        NoTls,
+    ).unwrap();
+
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS person (
+            id      SERIAL PRIMARY KEY,
+            name    TEXT NOT NULL,
+            age     INT NOT NULL
+        )",
+    ).unwrap();
+
+    client.execute(
+        "INSERT INTO person (name, age) VALUES ($1, $2)",
+        &[&person.name, &person.age],
+    ).unwrap();
+
+    let person_from_db = client.query_one(
+        "SELECT name, age FROM person WHERE id = 1",
+        &[],
+    ).unwrap();
+
+    let person_from_db = Person {
+        name: person_from_db.get(0),
+        age: person_from_db.get(1),
+    };
+
+    println!("person from db: {:?}", person_from_db);
 }
--- src/person.rs
+++ src/person.rs
@@ -3,5 +3,5 @@ use serde_derive::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Person {
     pub name: String,
-    pub age: u8,
+    pub age: i32,
 }
